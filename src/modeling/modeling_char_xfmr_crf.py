a3a2b75e389ebb1b93b88b87ca5d0f9b07188762
Amit Seker <aseker00@gmail.com>
2020-01-07 23:38:50 +0200

    Initialize CRF transition weights to reflect illegal transitions.

--- src/modeling/modeling_char_xfmr_crf.py
+++ src/modeling/modeling_char_xfmr_crf.py
@@ -5,11 +5,37 @@ from torchcrf import CRF
 import torch
 
 
+transition_matrix = {
+    'SOS': ['O', 'B-PER', 'B-LOC', 'B-ORG'],
+    'O': ['O', 'B-PER', 'B-LOC', 'B-ORG'],
+    'B-PER': ['I-PER'],
+    'B-LOC': ['I-LOC'],
+    'B-ORG': ['I-ORG'],
+    'I-PER': ['O', 'B-PER', 'B-LOC', 'B-ORG', 'I-PER'],
+    'I-LOC': ['O', 'B-PER', 'B-LOC', 'B-ORG', 'I-LOC'],
+    'I-ORG': ['O', 'B-PER', 'B-LOC', 'B-ORG', 'I-ORG'],
+}
+
+
 class CharXfmrCrfNerModel(CharXfmrNerModel):
 
-    def __init__(self, x_model: XfmrNerModel, ft_model: _FastText, char2id: dict, char_dropout_prob: float = 0.5):
+    def __init__(self, x_model: XfmrNerModel, ft_model: _FastText, char2id: dict, char_dropout_prob: float = 0.0):
         super(CharXfmrCrfNerModel, self).__init__(x_model, ft_model, char2id, char_dropout_prob)
         self.crf = CRF(self.num_labels, batch_first=True)
+        self._init_transition_weights()
+
+    def _init_transition_weights(self):
+        with torch.no_grad():
+            sos_nil_transitions = [self.label2id[label] for label in self.labels if label not in transition_matrix['SOS']]
+            for label_id in sos_nil_transitions:
+                self.crf.start_transitions[label_id] = -10000
+            for from_label in transition_matrix:
+                if from_label == 'SOS':
+                    continue
+                from_label_id = self.label2id[from_label]
+                from_nil_transitions = [self.label2id[label] for label in self.labels if label not in transition_matrix[from_label]]
+                for label_id in from_nil_transitions:
+                    self.crf.transitions[from_label_id, label_id] = -10000
 
     def forward(self, token_input_ids: torch.Tensor, token_attention_mask: torch.Tensor, char_token_idx: torch.Tensor,
                 char_input_ids: torch.Tensor, char_attention_mask: torch.Tensor, char_label_ids: torch.Tensor = None):
@@ -18,7 +44,8 @@ class CharXfmrCrfNerModel(CharXfmrNerModel):
         x_token_sequence_output = x_token_outputs[0]
         x_token_sequence_output = self.x_model.dropout(x_token_sequence_output)
         embedded_chars = self.char_emb(char_input_ids)
-        embedded_chars = self.char_dropout(embedded_chars)
+        if self.char_dropout > 0.0:
+            embedded_chars = self.char_dropout(embedded_chars)
         sequence_outputs = []
         for sent_idx in range(x_token_sequence_output.size(0)):
             sent_token_output = x_token_sequence_output[sent_idx]
